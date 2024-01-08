6f4e010dc5d8131f56acf41ef0876472ff8ff74d
ryanlaycock <ryan.laycock@hotmail.co.uk>
2024-01-08 23:30:08 +0000

    Customisable board and game settings

--- minespeeder-react-client/src/pages/Home.js
+++ minespeeder-react-client/src/pages/Home.js
@@ -1,18 +1,24 @@
 import axios from 'axios';
 import { v4 as uuidv4 } from 'uuid';
 import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 
 export default function Home() {
     const navigate = useNavigate();
-    
-    function CreateNewGame() {
+    const [numberOfBoards, setNumberOfBoards] = useState(2);
+    const [height, setHeight] = useState(8);
+    const [width, setWidth] = useState(8);
+    const [numberOfBombs, setNumberOfBombs] = useState(5);
+
+    function createNewGame(event) {
+        event.preventDefault();
         const id = uuidv4();
         axios.post('http://localhost:8080/v1/games', { 
-            "numberOfBoards": 2,
+            "numberOfBoards": parseInt(numberOfBoards),
             "boardOptions": {
-                "height": 8,
-                "width": 8,
-                "numberOfBombs": 5
+                "height": parseInt(height),
+                "width": parseInt(width),
+                "numberOfBombs": parseInt(numberOfBombs)
             },
             "id": id
         })
@@ -21,7 +27,7 @@ export default function Home() {
         })
         .catch(error => {
             console.error('There was an error creating new game', error);
-        });;
+        });
     }
 
     return (
@@ -34,9 +40,27 @@ export default function Home() {
         <p>
             Minespeeder is a work in progress. It's currently in a very early alpha state. 
             You can play it, but it's not very fun yet. 
-            If you want to try it out, click the button below to create a game.
+            If you want to try it out, fill the form below to create a game.
         </p>
-        <button onClick={CreateNewGame} >New game</button>
+        <form onSubmit={createNewGame}>
+            <label>
+                Number of Boards:
+                <input type="number" value={numberOfBoards} onChange={e => setNumberOfBoards(e.target.value)} />
+            </label>
+            <label>
+                Height:
+                <input type="number" value={height} onChange={e => setHeight(e.target.value)} />
+            </label>
+            <label>
+                Width:
+                <input type="number" value={width} onChange={e => setWidth(e.target.value)} />
+            </label>
+            <label>
+                Number of Bombs:
+                <input type="number" value={numberOfBombs} onChange={e => setNumberOfBombs(e.target.value)} />
+            </label>
+            <input type="submit" value="Create New Game" />
+        </form>
         </div>
     );
 }
